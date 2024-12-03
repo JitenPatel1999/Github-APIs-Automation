25da487f1fd27a3d09d5198d34e020c521a326ee
JitenPatel1999 <jitenpatel1999@gmail.com>
2024-12-03 14:37:58 -0500

    updated the token

--- src/test/java/Base/Constants.java
+++ src/test/java/Base/Constants.java
@@ -1,6 +1,6 @@
 package Base;
 
 public class Constants {
-	public final static String token = "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";
+	public final static String token = "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";
 	public final static String baseURI = "https://api.github.com";
 }
